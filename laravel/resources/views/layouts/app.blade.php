154e297314300db5f27360922d023646d91800f6
Seppo Puusa <seppo.puusa@gmail.com>
2026-01-22 11:57:50 +0700

    Add plausible analytics script

--- laravel/resources/views/layouts/app.blade.php
+++ laravel/resources/views/layouts/app.blade.php
@@ -137,6 +137,14 @@
                 }
             </script>
         @endif
+
+        <!-- Privacy-friendly analytics by Plausible -->
+        <script async src="https://plausible.io/js/pa-pDXhgmi5eZqoIa4scLqJt.js"></script>
+        <script>
+            window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};
+            plausible.init()
+        </script>
+
         @livewireStyles
     </head>
     <body class="font-sans antialiased bg-slate-50 min-h-screen flex flex-col">
